f81a6fbe0d05065deeb98d3cf8c04bf4824f8464
Pilar <nadaalpilarr@gmail.com>
2026-01-24 14:30:27 -0300

    js actualizado

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ new Swiper(".promoSwiper",{
     },
     direction: "horizontal",
 });
+
+//añadiendo funcionalidad al swiper de cards de productos
+
+const swiper = new Swiper(".mySwiper",{
+    slidePerview: 1,
+    spaceBetween: 20,
+    loop: true,
+
+    pagination:{
+        el: ".swiper-paginacion",
+        clikeable: true,
+
+    },
+
+    navigation:{
+        nextEl: ".swiperbutton-next",
+        prevEl: ".swiperbutton-prev",
+    },
+
+    breakpoints : {
+        640:{
+            slidePreview: 2,
+        },
+        1024:{
+            slidePerview: 4,
+        },
+    },
+});
